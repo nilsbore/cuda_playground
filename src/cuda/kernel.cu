258b30c21aad34e0c9ed85a553843416c175e399
Nils Bore <nilsbore@gmail.com>
2016-12-02 20:59:39 +0100

    Weirdly, this is working on my home computer

--- src/cuda/kernel.cu
+++ src/cuda/kernel.cu
@@ -4,7 +4,7 @@
 __global__ void my_first_kernel(float *x)
 {
     int tid = threadIdx.x + blockDim.x*blockIdx.x;
-    x[tid] = (float) 2.0f;//threadIdx.x;
+    x[tid] = (float) threadIdx.x;
 }
 
 void my_first(float *x, int nblocks, int nthreads)
